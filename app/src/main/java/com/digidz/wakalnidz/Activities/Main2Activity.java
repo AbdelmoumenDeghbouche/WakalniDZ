22c586449674d2c9625b27e6082798dfd524b3e8
minouesi <86883036+minouesi@users.noreply.github.com>
2022-09-01 12:12:11 +0100

    SplashScreenActivity Done

---
+++ app/src/main/java/com/digidz/wakalnidz/Activities/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.digidz.wakalnidz.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.digidz.wakalnidz.R.layout.activity_main2);
+    }
+}
---
+++ app/src/main/java/com/digidz/wakalnidz/Activities/SplashScreenActivity.java
@@ -0,0 +1,59 @@
+package com.digidz.wakalnidz.Activities;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsetsController;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.digidz.wakalnidz.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private RelativeLayout rel_get_started;
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.digidz.wakalnidz.R.layout.activity_main);
+        modifysupportactiobar();
+        initViews();
+
+        businessLogic();
+
+
+    }
+
+    private void businessLogic() {
+        rel_get_started.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO:Create Destination Activity
+                Intent intent = new Intent(SplashScreenActivity.this,SplashScreenActivity.class);
+                
+            }
+        });
+    }
+
+    private void initViews() {
+        rel_get_started =findViewById(R.id.rel_get_started);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void modifysupportactiobar() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        this.getWindow().setStatusBarColor(getColor(R.color.main_orange));
+
+        // to change the color of the icons in status bar to dark
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            this.getWindow().getDecorView().getWindowInsetsController().setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
+        }
+
+    }
+
+}
